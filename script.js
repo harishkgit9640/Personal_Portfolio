7454a15b7476b94b62b72bc583af3f00570748ec
Harish Kumar <harishstudio877@gmail.com>
2025-01-04 17:04:41 +0530

    some more changes has been done

--- script.js
+++ script.js
@@ -1,40 +1,43 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if(this.scrollY > 20){
+$(document).ready(function () {
+    let dt = new Date();
+    $('#year').html(dt.getFullYear());
+
+    $(window).scroll(function () {
+        if (this.scrollY > 20) {
             $('.navbar').addClass("sticky");
         }
-        else{
+        else {
             $('.navbar').removeClass("sticky");
         }
-        if(this.scrollY > 500){
+        if (this.scrollY > 500) {
             $('.scroll-up-btn').addClass("show");
         }
-        else{
+        else {
             $('.scroll-up-btn').removeClass("show");
         }
     });
 
     // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
+    $('.scroll-up-btn').click(function () {
+        $('html').animate({ scrollTop: 0 });
     });
 
     // menu toggle class
-    $('.menu-btn').click(function(){
+    $('.menu-btn').click(function () {
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
-    
+
     // typing Animation script
 
-    var typed = new Typed(".typing",{
-        strings: ["Graphic Designer","Web Developer", "Freelancer"],
+    var typed = new Typed(".typing", {
+        strings: ["Graphic Designer", "Web Developer", "Freelancer"],
         typeSpeed: 50,
         backSpeed: 60,
         loop: true
     });
-    var typed = new Typed(".typing-2",{
-        strings: ["Graphic Designer","Web Developer", "Freelancer",],
+    var typed = new Typed(".typing-2", {
+        strings: ["Graphic Designer", "Web Developer", "Freelancer",],
         typeSpeed: 50,
         backSpeed: 60,
         loop: true
@@ -47,16 +50,16 @@ $(document).ready(function(){
         autoplayTimeOut: 2000,
         autoplayHoverPause: true,
         responsive: {
-            0:{
-                items:  1,
+            0: {
+                items: 1,
                 nav: false
             },
-            600:{
-                items:  2,
+            600: {
+                items: 2,
                 nav: false
             },
-            1000:{
-                items:  3,
+            1000: {
+                items: 3,
                 nav: false
             }
         }
